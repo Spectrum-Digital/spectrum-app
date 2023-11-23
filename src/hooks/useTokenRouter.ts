429de10a1d70a09cdd5f3c3bec8e96ccbe32f84e
coinlordd <coinlord.developer@gmail.com>
2023-11-23 13:13:32 +0100

    fix url issue

--- src/hooks/useTokenRouter.ts
+++ src/hooks/useTokenRouter.ts
@@ -2,17 +2,13 @@ import { useMemo } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { Path, PathsValidator } from '@/lib/router'
 
-import { getBaseUrl } from '@/utils/url'
 import { MinimalToken } from '@/typings'
 
 export function useTokenRouter(tokenIn: MinimalToken, tokenOut: MinimalToken): Path[] {
   const { data } = useQuery({
     queryKey: ['tokenRouter', { tokenIn, tokenOut }],
     queryFn: async () => {
-      const url = new URL(`${getBaseUrl()}/api/router`)
-      url.searchParams.set('tokenIn', tokenIn.address)
-      url.searchParams.set('tokenOut', tokenOut.address)
-      url.searchParams.set('chainId', tokenOut.chainId.toString())
+      const url = `/api/router?tokenIn=${tokenIn.address}&tokenOut=${tokenOut.address}&chainId=${tokenIn.chainId}`
       const result = await fetch(url).then(res => res.json())
       const parsed = PathsValidator.safeParse(result)
       return parsed.success ? parsed.data : []
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,6 +2,10 @@ export function getBaseUrl() {
   if (typeof window !== 'undefined') {
     return `http://localhost:${process.env.PORT ?? 3000}`
   }
+  if (process.env.DOMAIN_URL) {
+    // optional reference for your domain
+    return `https://${process.env.DOMAIN_URL}`
+  }
   if (process.env.VERCEL_URL) {
     // reference for vercel.com
     return `https://${process.env.VERCEL_URL}`
